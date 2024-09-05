9fac5d3bb343d949841243c07e32e7c971422628
Ritesh Yadav <179428515+ritesh-auth@users.noreply.github.com>
2024-09-05 20:39:18 +0530

    Update apiConfig.js

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,6 +1,6 @@
 
-const ApiKey = 'mWqE2AjAbLZZNKBT3VDNCg';
-const domain = 'athena-751661737529874645';
+const ApiKey = 'FRESH_SALE API KEY';
+const domain = 'SUB_DOMAIN';
 const url = `https://${domain}.freshsales.io/api/contacts`;
 
 module.exports = {
